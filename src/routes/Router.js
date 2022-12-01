d22b11a8152a4705a8faa1223a27841430701e7a
taveirafelipe <taveira.felipe@gmail.com>
2022-12-01 13:37:52 -0300

    exercicios aula

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,47 +1,47 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { BASE_URL } from "../constants/url";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BASE_URL } from "../constants/url";
 import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import PokedexPage from "../pages/PokedexPage/PokedexPage";
 
 function Router() {
-  const [pokelist, setPokelist] = useState([]);
-  const [pokedex, setPokedex] = useState([]);
+  // const [pokelist, setPokelist] = useState([]);
+  // const [pokedex, setPokedex] = useState([]);
 
-  useEffect(() => {
-    fetchPokelist();
-  }, []);
+  // useEffect(() => {
+  //   fetchPokelist();
+  // }, []);
 
-  const fetchPokelist = async () => {
-    try {
-      const response = await axios.get(BASE_URL);
-      setPokelist(response.data.results);
-    } catch (error) {
-      console.log("Erro ao buscar lista de pokemons");
-      console.log(error.response);
-    }
-  };
+  // const fetchPokelist = async () => {
+  //   try {
+  //     const response = await axios.get(BASE_URL);
+  //     setPokelist(response.data.results);
+  //   } catch (error) {
+  //     console.log("Erro ao buscar lista de pokemons");
+  //     console.log(error.response);
+  //   }
+  // };
 
-  const addToPokedex = (pokemonToAdd) => {
-    const isAlreadyOnPokedex = pokedex.find(
-      (pokemonInPokedex) => pokemonInPokedex.name === pokemonToAdd.name
-    );
+  // const addToPokedex = (pokemonToAdd) => {
+  //   const isAlreadyOnPokedex = pokedex.find(
+  //     (pokemonInPokedex) => pokemonInPokedex.name === pokemonToAdd.name
+  //   );
 
-    if (!isAlreadyOnPokedex) {
-      const newPokedex = [...pokedex, pokemonToAdd];
-      setPokedex(newPokedex);
-    }
-  };
+  //   if (!isAlreadyOnPokedex) {
+  //     const newPokedex = [...pokedex, pokemonToAdd];
+  //     setPokedex(newPokedex);
+  //   }
+  // };
 
-  const removeFromPokedex = (pokemonToRemove) => {
-    const newPokedex = pokedex.filter(
-      (pokemonInPokedex) => pokemonInPokedex.name !== pokemonToRemove.name
-    );
+  // const removeFromPokedex = (pokemonToRemove) => {
+  //   const newPokedex = pokedex.filter(
+  //     (pokemonInPokedex) => pokemonInPokedex.name !== pokemonToRemove.name
+  //   );
 
-    setPokedex(newPokedex);
-  };
+  //   setPokedex(newPokedex);
+  // };
 
   return (
     <BrowserRouter>
@@ -50,9 +50,9 @@ function Router() {
           path="/"
           element={
             <HomePage
-              pokelist={pokelist}
-              addToPokedex={addToPokedex}
-              pokedex={pokedex}
+              // pokelist={pokelist}
+              // addToPokedex={addToPokedex}
+              // pokedex={pokedex}
             />
           }
         />
@@ -60,8 +60,8 @@ function Router() {
           path="/pokedex"
           element={
             <PokedexPage
-              pokedex={pokedex}
-              removeFromPokedex={removeFromPokedex}
+              // pokedex={pokedex}
+              // removeFromPokedex={removeFromPokedex}
             />
           }
         />
